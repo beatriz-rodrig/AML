1be2a587ce0cf3d4481d09f650782983bacfa784
beatriz-rodrig <beatriz_rodrigues1993@hotmail.com>
2022-03-02 15:52:22 -0300

    CP

---
+++ Bancodedados.sh
@@ -0,0 +1,7 @@
+wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202001"
+wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202002"
+wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202003"
+
+unzip 202001
+unzip 202002
+unzip 202003

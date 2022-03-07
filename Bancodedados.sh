2c4f5588e5fa711d88f1d43936cc98f3a78a5ecd
Beatriz Rodrigues <89103950+beatriz-rodrig@users.noreply.github.com>
2022-03-07 15:28:17 -0300

    Update Bancodedados.sh

--- Bancodedados.sh
+++ Bancodedados.sh
@@ -1,163 +1,114 @@
-!#/bin/bash
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201601"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201602"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201603"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201604"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201605"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201606"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201607"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201608"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201609"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201610"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201611"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201612"
+#!/bin/bash
 
-unzip 201601
-unzip 201602
-unzip 201603
-unzip 201604
-unzip 201605
-unzip 201606
-unzip 201607
-unzip 201608
-unzip 201609
-unzip 201610
-unzip 201611
-unzip 201612
+#Download de dados, extrair e remover zip
+function downloadcpgf () {
+for ano in {17..21}
+do
+for mes in {01..12}
+do
 
-!#/bin/bash
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201701"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201702"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201703"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201704"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201705"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201706"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201707"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201708"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201709"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201710"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201711"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201712"
+#Download dos arquivos
+wget --read-timeout=5 -P /home/beatriz.rodrigues/Downloads "https://
+www.portaltransparencia.gov.br/download-de-dados/cpgf/20$ano$mes"
+
+unzip /home/beatriz.rodrigues/Downloads/20$ano$mes -d /home/beatriz.rodrigues/Downloads
+rm /home/beatriz.rodrigues/Downloads/20$ano$mes
+done
+done
+}
 
 
-unzip 201701
-unzip 201702
-unzip 201703
-unzip 201704
-unzip 201705
-unzip 201706
-unzip 201707
-unzip 201708
-unzip 201709
-unzip 201710
-unzip 201711
-unzip 201712
+function tabelas () {
+#Criando tabelas
+PGPASSWORD=aml psql -U aml -d aml --c "
+drop table if exists public."tmp_cpgf";
+CREATE TABLE public."tmp_cpgf" (
+"codigo_orgao_superior" integer NULL,
+"nome_orgao_superior" varchar(1024) NULL,
+"codigo_orgao" integer NULL,
+"nome_orgao" varchar(1024) NULL,
+"codigo_unidade_gestora" integer NULL,
+"nome_unidade_gestora" varchar(1024) NULL,
+"ano" integer NULL,
+"mes" integer NULL,
+"cpf_portador" varchar(1024) NULL,
+"nome_portador" varchar(1024) NULL,
+"documento_favorecido" varchar(1024) NULL,
+"nome_favorecido" varchar(1024) NULL,
+"transacao" varchar(1024) NULL,
+"data" varchar(1024) NULL,
+"valor" varchar(1024) NULL
+);
+drop table if exists public."final_cpgf";
+CREATE TABLE public."final_cpgf" (
+"codigo_orgao_superior" integer NULL,
+"nome_orgao_superior" varchar(1024) NULL,
+"codigo_orgao" integer NULL,
+"nome_orgao" varchar(1024) NULL,
+"codigo_unidade_gestora" integer NULL,
+"nome_unidade_gestora" varchar(1024) NULL,
+"ano" integer NULL,
+"mes" integer NULL,
+"cpf_portador" varchar(1024) NULL,
+"nome_portador" varchar(1024) NULL,
+"documento_favorecido" varchar(1024) NULL,
+"nome_favorecido" varchar(1024) NULL,
+"transacao" varchar(1024) NULL,
+"data" date NULL,
+"valor" decimal(20,2) NULL
+);"
+#Copying data to tmp table
+for ano in {17..21}
+do
+for mes in {01..12}
+do
 
-!#/bin/bash
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201801"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201802"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201803"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201804"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201805"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201806"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201807"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201808"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201809"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201810"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201811"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201812"
+PGPASSWORD=aml psql -U aml -d aml -c "\copy tmp_cpgf from '/home/beatriz.rodrigues/Downloads/20${ano}${mes}_CPGF.csv' csv header delimiter ';' encoding 'latin1'"
 
-unzip 201801
-unzip 201802
-unzip 201803
-unzip 201804
-unzip 201805
-unzip 201806
-unzip 201807
-unzip 201808
-unzip 201809
-unzip 201810
-unzip 201811
-unzip 201812
+done
+done
+}
+function tabelafinal () {
+psql -U aml -d aml -c
+"insert into final_cpgf(codigo_orgao_superior, nome_orgao_superior, codigo_orgao, nome_orgao, codigo_unidade_gestora, 
+	nome_unidade_gestora, ano, mes, cpf_portador, documento_favorecido, nome_favorecido, transacao, nome_portador, "data", valor)
+	select
+	codigo_orgao_superior,
+	nome_orgao_superior,
+	codigo_orgao,
+	nome_orgao,
+	codigo_unidade_gestora,
+	nome_unidade_gestora,
+	ano,
+	mes,
+	cpf_portador,
+	documento_favorecido,
+	nome_favorecido,
+	transacao,
+	nome_portador,
+	TO_DATE("data", 'DD/MM/YYYY') as "data",
+	cast(replace(valor, ',', '.' ) as decimal(20,2)) as valor
+	from tmp_cpgf;"
+}
 
-!#/bin/bash
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201901"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201902"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201903"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201904"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201905"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201906"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201907"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201908"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201909"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201910"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201911"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/201912"
+cmds='downloadcpgf  tabelas tabelafinal Sair'
 
-unzip 201901
-unzip 201902
-unzip 201903
-unzip 201904
-unzip 201905
-unzip 201906
-unzip 201907
-unzip 201908
-unzip 201909
-unzip 201910
-unzip 201911
-unzip 201912
-
-!#/bin/bash
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202001"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202002"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202003"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202004"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202005"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202006"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202007"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202008"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202009"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202010"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202011"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202012"
-
-unzip 202001
-unzip 202002
-unzip 202003
-unzip 202004
-unzip 202005
-unzip 202006
-unzip 202007
-unzip 202008
-unzip 202009
-unzip 202010
-unzip 202011
-unzip 202012
-
-!#/bin/bash
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202101"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202102"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202103"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202104"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202105"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202106"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202107"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202108"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202109"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202110"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202111"
-wget "https://www.portaltransparencia.gov.br/download-de-dados/cpgf/202112"
+select cmd in $cmds
+do
+if [ $cmd == 'Sair' ]
+then break
+elif [[ "$cmd" == "downloadcpgf" ]]; then
+downloadcpgf
+elif [[ "$cmd" == "tabelas" ]]; then
+tabelas
+elif [[ "$cmd" == "tabelafinal" ]]; then
+tabelafinal
+else
+echo "Comando não encontrado."
+fi
+done
 
 
-unzip 202101
-unzip 202102
-unzip 202103
-unzip 202104
-unzip 202105
-unzip 202106
-unzip 202107
-unzip 202108
-unzip 202109
-unzip 202110
-unzip 202111
-unzip 202112
+
+
+
